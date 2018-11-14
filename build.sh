7235c73642d14496edc11d8f5683dca1bedf8ae3
HuyVQ <huyquv@gmail.com>
2018-11-14 09:13:33 +0700

    fix

--- build.sh
+++ build.sh
@@ -26,10 +26,8 @@ export LD_LIBRARY_PATH=/usr/local/lib/:/usr/local/lib64/:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig/
 
 cp -r fonts/ /usr/share/fonts
+tar -xvf ffmpeg.2.8.13.taz.gz -C /
+
 git clone https://github.com/quanghuy1288/gpac.git /opt/gpac
 
 cd /opt/gpac && ./configure --disable-wx --strip --prefix=/usr/local && make && make install && make clean
-
-tar -xvf ffmpeg.2.8.13.taz.gz -C /
-
-
